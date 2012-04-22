5ee7865458aa386a585aceb2fc4ecb555c18be89
Dan Hirsch <thequux@thequux.com>
2012-04-23 00:35:58 +0100

    Added allocator and first cut at makefile

---
+++ lib/allocator.h
@@ -0,0 +1,12 @@
+#ifndef LIB_ALLOCATOR__H__
+#define LIB_ALLOCATOR__H__
+#include <sys/types.h>
+
+typedef struct arena* arena_t; // hidden implementation
+
+arena_t new_arena(size_t block_size); // pass 0 for default...
+void* arena_malloc(arena_t arena, size_t count); // TODO: needs the malloc attribute
+void delete_arena(arena_t arena);
+
+
+#endif // #ifndef LIB_ALLOCATOR__H__
