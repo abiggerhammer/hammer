8bf9f7ae6247d79028e904e0d045f2bd9480ab0a
Meredith L. Patterson <clonearmy@gmail.com>
2012-05-11 15:26:04 +0100

    registered parser tests in test suite

--- src/test_suite.c
+++ src/test_suite.c
@@ -2,12 +2,14 @@
 #include "test_suite.h"
 
 extern void register_bitreader_tests();
+extern void register_parser_tests();
 
 int main(int argc, char** argv) {
   g_test_init(&argc, &argv, NULL);
 
   // register various test suites...
   register_bitreader_tests();
+  register_parser_tests();
 
   g_test_run();
 }
