fab8705828b5e13ac15ba87c8a7ff995cf7063be
Sven M. Hallberg <pesco@khjk.org>
2013-01-13 18:56:45 +0100

    typo

--- examples/base64_sem1.c
+++ examples/base64_sem1.c
@@ -6,7 +6,7 @@
 //
 // Note how the grammar is defined by using the macros H_RULE and H_ARULE.
 // Those rules using ARULE get an attached action which must be declared (as
-// (a function of type HAction) with a standard name based on the rule name.
+// a function of type HAction) with a standard name based on the rule name.
 //
 // This variant of the example uses fine-grained semantic actions that
 // transform the parse tree in small steps in a bottom-up fashion. Compare
