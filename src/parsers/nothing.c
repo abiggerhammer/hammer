e69fd2e5b9f71b650bc26b61316842560668255d
Rob Zinkov <rob@zinkov.com>
2013-03-18 15:27:16 -0700

    Adding epislon and nothing

--- src/parsers/nothing.c
+++ src/parsers/nothing.c
@@ -1,15 +1,20 @@
 #include "parser_internal.h"
 
-
 static HParseResult* parse_nothing() {
   // not a mistake, this parser always fails
   return NULL;
+}
+
+static bool nothing_ctrvm(HRVMProg *prog, void* env) {
+  h_rvm_insert_insn(prog, RVM_MATCH, 0x00FF);
+  return true;
 }
 
 static const HParserVtable nothing_vt = {
   .parse = parse_nothing,
   .isValidRegular = h_true,
   .isValidCF = h_true,
+  .compile_to_rvm = nothing_ctrvm,
 };
 
 const HParser* h_nothing_p() {
