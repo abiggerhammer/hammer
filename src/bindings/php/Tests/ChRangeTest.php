282c42139db54d5760bce8017bed4337ddeaadbc
Meredith L. Patterson <mlp@thesmartpolitenerd.com>
2013-11-24 20:30:19 -0600

    ChTest and ChRangeTest fail only because their result isn't being coerced to string.

---
+++ src/bindings/php/Tests/ChRangeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'hammer.php';
+
+class ChRangeTest extends PHPUnit_Framework_TestCase 
+{
+    protected $parser;
+
+    protected function setUp() 
+    {
+        $this->parser = h_ch_range("a", "c");
+    }
+    public function testSuccess() 
+    {
+        $result = h_parse($this->parser, "b");
+        $this->assertEquals("b", $result);
+    }     
+    public function testFailure()
+    {
+        $result = h_parse($this->parser, "d");
+        $this->assertEquals(NULL, $result);
+    }
+}
+?>
