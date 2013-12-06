28a8c02b74fe03bffd06aed05d6e0ebf7a25248f
Meredith L. Patterson <mlp@thesmartpolitenerd.com>
2013-12-06 13:31:32 +0100

    building libjhammer.so works. now for a jar

--- src/bindings/java/jsrc/Action.java
+++
@@ -1,8 +0,0 @@
-package com.upstandinghackers.hammer;
-
-import java.util.List;
-
-public interface Action
-{
-    public List<ParsedToken> execute(ParseResult p);
-}
--- src/bindings/java/jsrc/Predicate.java
+++
@@ -1,6 +0,0 @@
-package com.upstandinghackers.hammer;
-
-public interface Predicate
-{
-    public boolean apply(ParseResult p);
-}
