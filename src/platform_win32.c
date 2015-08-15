2346239887216e2d6d4eb2c34914811203b64bc5
Nicolas Léveillé <nicolas@uucidl.com>
2015-08-15 13:47:34 +0200

    Implement h_platform_stopwatch for Windows
    We use QueryPerformanceCounter which will return realtime, not user
    time.

--- src/platform_win32.c
+++ src/platform_win32.c
@@ -8,3 +8,15 @@ void h_platform_errx(int err, const char* format, ...) {
   ExitProcess(err);
 }
 
+void h_platform_stopwatch_reset(struct HStopWatch* stopwatch) {
+  QueryPerformanceFrequency(&stopwatch->qpf);
+  QueryPerformanceCounter(&stopwatch->start);
+}
+
+/* return difference between last reset point and now */
+int64_t h_platform_stopwatch_ns(struct HStopWatch* stopwatch) {
+  LARGE_INTEGER now;
+  QueryPerformanceCounter(&now);
+
+  return 1000000000 * (now.QuadPart - stopwatch->start.QuadPart) / stopwatch->qpf.QuadPart;
+}
